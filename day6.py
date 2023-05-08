f86dbabf14e081cb3d6bbd3a893a088e9371c6f6
sebastiansunny <132342122+sebastiansunny@users.noreply.github.com>
2023-05-08 12:12:05 +0530

    Update day6.py

--- day6.py
+++ day6.py
@@ -1,6 +1,6 @@
 print("biodata of student ")
 name = "sebastian sunny"
-age=17
+age=21
 college="GPTC CHELAKKARA"
 pla="wadakancherry"
 print("self intro- \n" ,"my name is", name  ,   " \n",  age ," years  old \n","iam form",college,"\n","myplace is",pla)
