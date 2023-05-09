53667253f90504797db5b748376777438e09bc8c
sebastiansunny <132342122+sebastiansunny@users.noreply.github.com>
2023-05-09 14:50:15 +0530

    Create pythonexp3.py

---
+++ pythonexp3.py
@@ -0,0 +1,29 @@
+a=float(input("enter first number"))
+b=float(input("enter the second number"))
+print("addition")
+def add(a,b):
+    c=a+b
+    return c
+v=add(a,b)
+print(v)
+print("multiply")
+def multiply(a,b):
+    d=a*b
+    return d
+f=multiply(a,b)
+print(f)
+print("div")
+def div(a,b):
+    d=a/b
+    return d
+d=div(a,b)
+print(d)
+print("substraction")
+def substraction(a,b):
+    d=a-b
+    return d
+g=substraction(a,b)
+print(g)
+    
+    
+    
